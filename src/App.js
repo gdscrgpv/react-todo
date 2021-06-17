c2ba27cd83e31da08062278af8a45ebe3952d484
Apoorv Pandey <32810722+apoorvpandey0@users.noreply.github.com>
2021-06-17 12:46:25 +0530

    ID's increment by 1 for each new todo

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import "./styles.css";
-
+import React from "react";
 export default function App() {
-  const todos = [
+  const [todos, setTodos] = React.useState([
     { id: 1, text: "Wash dishes", done: false },
     { id: 2, text: "Do laundry", done: false },
     { id: 3, text: "Take shower", done: false }
-  ];
+  ]);
 
   return (
     <div>
       <h1>Todo List</h1>
       <TodoList todos={todos} />
-      <AddTodo />
+      <AddTodo setTodos={setTodos} length={todos.length} />
     </div>
   );
 }
 
 function TodoList(props) {
   console.log(props);
+  var todos = props.todos;
   return (
     <div>
-      {props.todos.map((todo) => (
+      {todos.map((todo) => (
         <li key={todo.id}>{todo.text}</li>
       ))}
     </div>
   );
 }
 
-function AddTodo() {
+function AddTodo({ setTodos, length }) {
+  const inputRef = React.useRef();
   function handleAddTodo(event) {
     // To prevent form default behaviour i.e page refresh on submit form
     event.preventDefault();
@@ -37,10 +39,20 @@ function AddTodo() {
 
     // this contains the actual value by using the name given to the field
     console.log(event.target.elements.addTodo.value);
+    const text = event.target.elements.addTodo.value;
+    const todo = {
+      id: ++length,
+      text,
+      done: false
+    };
+    setTodos((prevTodos) => {
+      return prevTodos.concat(todo);
+    });
+    inputRef.current.value = "";
   }
   return (
     <form onSubmit={handleAddTodo}>
-      <input name="addTodo" placeholder="Add todo" />
+      <input name="addTodo" placeholder="Add todo" ref={inputRef} />
       <button type="submit">Submit</button>
     </form>
   );
